21b31185c4b7be47f60562a98c6c51968a70de98
Luke Robinson <lukerobinson@lukes-mbp.chattanoogatn.gov>
2017-09-06 15:39:05 -0400

    final lab 2

--- script.js
+++ script.js
@@ -2,10 +2,13 @@ var friends = ['Chandler', 'Sam', 'Forrest', 'Avery', 'Bailey'];
 
 for (j = 0; j < friends.length; j++) {
     console.log(friends[j]);
-    for (i = 9; i > 0; i--) {
-        console.log((i + 1) + " lines of code in the file, " + (i + 1) + " lines of code; " + friends[j] + " strikes one out, clears it all out, " + i + "lines of code in the file.");
+    for (i = 9; i > -1; i--) {
+        console.log((i + 1) + " lines of code in the file, " + (i + 1) + " lines of code; " + friends[j] + " strikes one out, clears it all out, " + i + " lines of code in the file.");
         if (i === 1) {
             console.log((i + 1) + " lines of code in the file, " + (i + 1) + " lines of code; " + friends[j] + " strikes one out, clears it all out, " + i + " line of code in the file.");
+        }
+        if (i === 0) {
+            console.log((i + 1) + " line of code in the file, " + (i + 1) + " line of code; " + friends[j] + " strikes one out, clears it all out, " + "no more lines of code in the file.");
         }
     }
 }
